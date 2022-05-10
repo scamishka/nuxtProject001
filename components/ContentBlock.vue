8797ef10a8f3e2bd3ff127b920f6d1b295417e54
Mikhail <m.kudrinskiy@gmail.com>
2022-05-10 12:29:42 +0300

    first commit

---
+++ components/ContentBlock.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    content-block
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ContentBlock"
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="body-block">
+    <div class="body-block__container">
+      <HeaderTitle/>
+      <ContentBlock/>
+    </div>
+  </div>
+</template>
+
+<script>
+import HeaderTitle from "../components/HeaderTitle";
+import ContentBlock from "../components/ContentBlock";
+export default {
+  name: 'IndexPage',
+  components: {ContentBlock, HeaderTitle}
+}
+</script>
+
+<style lang="scss" scoped>
+.body-block {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  &__container {
+    width: 100%;
+    max-width: 1500px;
+    display: flex;
+    flex-direction: column;
+
+  }
+}
+
+</style>
